8d21b479da3b37a6d1a33c8e7f159b2cbb6afbab
vnam0320 <vnam0320@gmail.com>
2022-04-08 21:02:31 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace MyGame2468
+{
+    partial class FMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FMain));
+            this.PlayYard = new System.Windows.Forms.PictureBox();
+            this.pictureBox18 = new System.Windows.Forms.PictureBox();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox19 = new System.Windows.Forms.PictureBox();
+            this.ScoreBoard = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayYard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox18)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox19)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PlayYard
+            // 
+            this.PlayYard.BackColor = System.Drawing.Color.LightCoral;
+            this.PlayYard.Location = new System.Drawing.Point(12, 182);
+            this.PlayYard.Name = "PlayYard";
+            this.PlayYard.Size = new System.Drawing.Size(230, 230);
+            this.PlayYard.TabIndex = 0;
+            this.PlayYard.TabStop = false;
+            // 
+            // pictureBox18
+            // 
+            this.pictureBox18.BackgroundImage = global::MyGame2468.Properties.Resources.bg_num_pad;
+            this.pictureBox18.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox18.Location = new System.Drawing.Point(12, 21);
+            this.pictureBox18.Name = "pictureBox18";
+            this.pictureBox18.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox18.TabIndex = 17;
+            this.pictureBox18.TabStop = false;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.BackColor = System.Drawing.Color.IndianRed;
+            this.btnNewGame.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnNewGame.Location = new System.Drawing.Point(167, 138);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(75, 23);
+            this.btnNewGame.TabIndex = 18;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = false;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.LightPink;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(46, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 30);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "W";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.LightPink;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(47, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 30);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "S";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.LightPink;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(15, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 30);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "A";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.LightPink;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(77, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 30);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "D";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox19
+            // 
+            this.pictureBox19.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox19.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox19.BackgroundImage")));
+            this.pictureBox19.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox19.Location = new System.Drawing.Point(141, 16);
+            this.pictureBox19.Name = "pictureBox19";
+            this.pictureBox19.Size = new System.Drawing.Size(116, 109);
+            this.pictureBox19.TabIndex = 23;
+            this.pictureBox19.TabStop = false;
+            // 
+            // ScoreBoard
+            // 
+            this.ScoreBoard.BackColor = System.Drawing.Color.SlateBlue;
+            this.ScoreBoard.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ScoreBoard.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.ScoreBoard.Location = new System.Drawing.Point(152, 77);
+            this.ScoreBoard.Name = "ScoreBoard";
+            this.ScoreBoard.Size = new System.Drawing.Size(94, 23);
+            this.ScoreBoard.TabIndex = 24;
+            this.ScoreBoard.Text = "score: ";
+            // 
+            // FMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(255)))), ((int)(((byte)(245)))));
+            this.BackgroundImage = global::MyGame2468.Properties.Resources.bg;
+            this.ClientSize = new System.Drawing.Size(261, 424);
+            this.Controls.Add(this.ScoreBoard);
+            this.Controls.Add(this.pictureBox19);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.pictureBox18);
+            this.Controls.Add(this.PlayYard);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximumSize = new System.Drawing.Size(360, 640);
+            this.Name = "FMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "2048";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this._keyboardEvent);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.FMain_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.FMain_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.PlayYard)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox18)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox19)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PlayYard;
+        private System.Windows.Forms.PictureBox pictureBox18;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox19;
+        private System.Windows.Forms.Label ScoreBoard;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,432 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyGame2468
+{
+    public partial class FMain : Form
+    {
+        private int[,] Map;
+        private Label[,] Labels;
+        private PictureBox[,] Pics;
+        private int score;
+
+        public FMain()
+        {
+            // game over
+            InitializeComponent();
+            score = 0;
+            CreateMap();
+            Map = new int[4,4];
+            Labels = new Label[4,4];
+            Pics = new PictureBox[4,4];
+            GenerateRandom();
+        }
+
+        private void _keyboardEvent(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode.ToString())
+            {
+                case "D":
+                    {
+
+                        for (int x = 0; x < 4; x++)
+                        {
+                            int block = 0;
+                            for (int y = 3; y >= 0; y--)
+                            {
+                                if (Map[x, y] != 0)
+                                {
+                                    for (int j = y + 1; j < 4 - block; j++)
+                                    {
+                                        if (Map[x, j] == 0)
+                                        {
+                                            (Map[x, j], Map[x, j - 1]) = (Map[x, j - 1], Map[x, j]);
+                                            (Pics[x, j], Pics[x, j - 1]) = (Pics[x, j - 1], Pics[x, j]);
+                                            (Labels[x, j], Labels[x, j - 1]) = (Labels[x, j - 1], Labels[x, j]);
+
+                                            Pics[x, j].Location = new Point(Pics[x, j].Location.X + 56, Pics[x, j].Location.Y);
+                                        }
+                                        else
+                                        {
+                                            int a = Map[x, j];
+                                            int b = Map[x, j - 1];
+
+                                            if (a == b)
+                                            {
+                                                
+                                                Map[x, j] = a + b;
+                                                Labels[x, j].Text = Map[x, j].ToString();
+                                                Pics[x, j].BackColor = GetColors(Map[x, j]);
+
+                                                score += a + b;
+                                                ScoreBoard.Text = $"score: {score}";
+
+                                                Map[x, j - 1] = 0;
+                                                PlayYard.Controls.Remove(Pics[x, j - 1]);
+                                                PlayYard.Controls.Remove(Labels[x, j - 1]);
+                                                Pics[x, j - 1] = null;
+                                                Labels[x, j - 1] = null;
+
+                                            }
+
+                                            block++;
+                                        }
+
+                                    }
+                                }
+                            }
+                        }
+
+                        GenerateRandom();
+                        break;
+                    }
+                case "A":
+                    {
+                        for (int x = 0; x < 4; x++)
+                        {
+                            int block = 0;
+                            for (int y = 0; y < 4; y++)
+                            {
+                                if (Map[x, y] != 0)
+                                {
+                                    for (int j = y - 1; j >= 0+block; j--)
+                                    {
+                                        if (Map[x, j] == 0)
+                                        {
+                                            (Map[x, j], Map[x, j + 1]) = (Map[x, j + 1], Map[x, j]);
+                                            (Pics[x, j], Pics[x, j + 1]) = (Pics[x, j + 1], Pics[x, j]);
+                                            (Labels[x, j], Labels[x, j + 1]) = (Labels[x, j + 1], Labels[x, j]);
+
+                                            Pics[x, j].Location = new Point(Pics[x, j].Location.X - 56, Pics[x, j].Location.Y);
+                                        }
+                                        else
+                                        {
+                                            int a = Map[x, j];
+                                            int b = Map[x, j + 1];
+
+                                            if (a == b)
+                                            {
+                                                Map[x, j] = a + b;
+                                                Labels[x, j].Text = Map[x, j].ToString();
+                                                Pics[x, j].BackColor = GetColors(Map[x, j]);
+
+                                                score += a + b;
+                                                ScoreBoard.Text = $"score: {score}";
+
+                                                Map[x, j + 1] = 0;
+                                                PlayYard.Controls.Remove(Pics[x, j + 1]);
+                                                PlayYard.Controls.Remove(Labels[x, j + 1]);
+                                                Pics[x, j + 1] = null;
+                                                Labels[x, j + 1] = null;
+
+                                            }
+                                            block++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        GenerateRandom();
+                        break;
+                    }
+
+                case "S":
+                    {
+                        for (int y = 0; y < 4; y++)
+                        {
+                            int block = 0;
+                            for (int x = 3; x >= 0; x--)
+                            {
+                                if (Map[x, y] != 0)
+                                {
+                                    for (int i = x + 1; i < 4-block; i++)
+                                    {
+                                        if (Map[i, y] == 0)
+                                        {
+                                            (Map[i, y], Map[i - 1, y]) = (Map[i - 1, y], Map[i, y]);
+                                            (Pics[i, y], Pics[i - 1, y]) = (Pics[i - 1, y], Pics[i, y]);
+                                            (Labels[i, y], Labels[i - 1, y]) = (Labels[i - 1, y], Labels[i, y]);
+
+                                            Pics[i, y].Location = new Point(Pics[i, y].Location.X, Pics[i, y].Location.Y + 56);
+                                        }
+                                        else
+                                        {
+                                            int a = Map[i, y];
+                                            int b = Map[i - 1, y];
+
+                                            if (a == b)
+                                            {
+                                                Map[i, y] = a + b;
+                                                Labels[i, y].Text = Map[i, y].ToString();
+                                                Pics[i, y].BackColor = GetColors(Map[i, y]);
+
+                                                score += a + b;
+                                                ScoreBoard.Text = $"score: {score}";
+
+                                                Map[i - 1, y] = 0;
+                                                PlayYard.Controls.Remove(Pics[i - 1, y]);
+                                                PlayYard.Controls.Remove(Labels[i - 1, y]);
+                                                Pics[i - 1, y] = null;
+                                                Labels[i - 1, y] = null;
+
+                                            }
+                                            block++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        GenerateRandom();
+                        break;
+                    }
+
+                case "W":
+                    {
+                        for (int y = 0; y < 4; y++)
+                        {
+                            int block = 0;
+                            for (int x = 0; x <= 3; x++)
+                            {
+                                if (Map[x, y] != 0)
+                                {
+                                    for (int i = x - 1; i >= 0+block; i--)
+                                    {
+                                        if (Map[i, y] == 0)
+                                        {
+                                            (Map[i, y], Map[i + 1, y]) = (Map[i + 1, y], Map[i, y]);
+                                            (Pics[i, y], Pics[i + 1, y]) = (Pics[i + 1, y], Pics[i, y]);
+                                            (Labels[i, y], Labels[i + 1, y]) = (Labels[i + 1, y], Labels[i, y]);
+
+                                            Pics[i, y].Location = new Point(Pics[i, y].Location.X, Pics[i, y].Location.Y - 56);
+                                        }
+                                        else
+                                        {
+                                            int a = Map[i, y];
+                                            int b = Map[i + 1, y];
+
+                                            if (a == b)
+                                            {
+                                                Map[i, y] = a + b;
+                                                Labels[i, y].Text = Map[i, y].ToString();
+                                                Pics[i, y].BackColor = GetColors(Map[i, y]);
+
+                                                score += a + b;
+                                                ScoreBoard.Text = $"score: {score}";
+
+                                                Map[i + 1, y] = 0;
+                                                PlayYard.Controls.Remove(Pics[i + 1, y]);
+                                                PlayYard.Controls.Remove(Labels[i + 1, y]);
+                                                Pics[i + 1, y] = null;
+                                                Labels[i + 1, y] = null;
+
+
+                                            }
+                                            block++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        GenerateRandom();
+                        break;
+                    }
+            }
+        }
+        private Color GetColors(int number)
+        {
+            switch (number)
+            {
+                case 2:
+                    return Color.FromArgb(250, 219, 216);
+                case 4:
+                    return Color.FromArgb(241, 148, 138);
+                case 8:
+                    return Color.FromArgb(236, 112, 99);
+                case 16:
+                    return Color.FromArgb(175, 122, 197);
+                case 32:
+                    return Color.FromArgb(125, 60, 152);
+                case 64:
+                    return Color.FromArgb(118, 215, 196);
+                case 128:
+                    return Color.FromArgb(125, 206, 160);
+                case 256:
+                    return Color.FromArgb(88, 214, 141);
+                case 512:
+                    return Color.FromArgb(244, 208, 63);
+                case 1024:
+                    return Color.FromArgb(243, 156, 18);
+                case 2048:
+                    return Color.FromArgb(230, 126, 34);
+                case 4096:
+                    return Color.FromArgb(223, 255, 0);
+                case 8192:
+                    return Color.FromArgb(222, 49, 99);
+                default:
+                    return Color.FromArgb(0, 0, 0);
+            }
+        }
+        private void CreateMap()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    PictureBox mapBG = new PictureBox
+                    {
+                        Location = new Point(6 + 56 * i, 6 + 56 * j),
+                        Size = new Size(50, 50),
+                        BackColor = Color.Silver
+                    };
+                    PlayYard.Controls.Add(mapBG);
+                }
+            }
+
+        }
+        private bool CheckGameIsOver()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (i >= 1)
+                        if (Map[i, j] == Map[i - 1, j])
+                            return true;
+                    if (i <=2 )
+                        if (Map[i, j] == Map[i + 1, j])
+                            return true;
+                    if  (j >= 1)
+                        if (Map[i, j] == Map[i, j - 1])
+                            return true;
+                    if (j <= 2)
+                        if (Map[i, j] == Map[i, j + 1])
+                            return true;
+                }
+            }
+            return false;
+        }
+        private bool GetFreePlace(out Tuple<int,int> randomFreePlace)
+        {
+            List<Tuple<int,int>> FreePlaces = new List<Tuple<int,int>>();
+            for(int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (Map[i, j] == 0)
+                        FreePlaces.Add(new Tuple<int,int>(i,j));
+                }
+            }
+            try
+            {
+                randomFreePlace = FreePlaces[new Random().Next(0, FreePlaces.Count)];
+            }
+            catch
+            {
+                randomFreePlace = new Tuple<int, int>(-1, -1);
+                return false;
+            }
+            return FreePlaces.Count != 0;
+        }
+        private void GenerateRandom()
+        {
+            Tuple<int, int> randomFreePlace = new Tuple<int, int>(-1, -1);
+
+            if (!GetFreePlace(out randomFreePlace))
+            {
+                if(!CheckGameIsOver())
+                {
+                    MessageBox.Show("Game Over");
+                    return;
+                }
+                else
+                {
+                    return;
+                }
+                
+            }
+
+            int x = randomFreePlace.Item1;
+            int y = randomFreePlace.Item2;
+
+            Map[x, y] = 2;
+            Pics[x, y] = new PictureBox();
+
+            Labels[x, y] = new Label()
+            {
+                Text = "2",
+                ForeColor = Color.White,
+                AutoSize = false,
+                TextAlign = ContentAlignment.MiddleCenter,
+                Font = new Font(new FontFamily("Microsoft Sans Serif"), 12),
+                Dock = DockStyle.Fill
+            };
+
+            Pics[x, y].Controls.Add(Labels[x, y]);
+            Pics[x, y].Location = new Point(6 + 56 * y, 6 + 56 * x);
+            Pics[x, y].Size = new Size(50, 50);
+            Pics[x, y].BackColor = Color.FromArgb(245, 183, 177);
+
+            PlayYard.Controls.Add(Pics[x, y]);
+            Pics[x, y].BringToFront();
+            
+        }
+        private void FMain_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar)
+            {
+                case 's':
+                    label3.ForeColor = Color.FromArgb(169, 50, 38);
+                    break;
+                case 'a':
+                    label4.ForeColor = Color.FromArgb(169, 50, 38);
+                    break;
+                case 'd':
+                    label2.ForeColor = Color.FromArgb(169, 50, 38);
+                    break;
+                case 'w':
+                    label1.ForeColor = Color.FromArgb(169, 50, 38);
+                    break;
+            }
+        }
+
+        private void FMain_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode.ToString())
+            {
+                case "S":
+                    label3.ForeColor = Color.White;
+                    break;
+                case "A":
+                    label4.ForeColor = Color.White;
+                    break;
+                case "D":
+                    label2.ForeColor = Color.White;
+                    break;
+                case "W":
+                    label1.ForeColor = Color.White;
+                    break;
+            }
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            score = 0;
+            Map = new int[4, 4];
+            Labels = new Label[4, 4];
+            Pics = new PictureBox[4, 4];
+            PlayYard.Controls.Clear();
+            CreateMap();
+            GenerateRandom();
+        }
+    }
+}
